a69d0b7d5453e65b068b0f66d37eeeeb3c2c1872
huangqishang <768432905@qq.com>
2025-11-06 11:27:36 +0800

    6次修改yml

--- docs/.vuepress/.temp/git/config.js
+++ docs/.vuepress/.temp/git/config.js
@@ -1,5 +1,5 @@
-import { GitContributors } from "F:/ownProject/huangjia/github-project/vue-press-02/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "F:/ownProject/huangjia/github-project/vue-press-02/vuepress-starter/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
+import { GitContributors } from "F:/ownProject/huangjia/github-project/vue-press-02/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
+import { GitChangelog } from "F:/ownProject/huangjia/github-project/vue-press-02/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
 
 export default {
   enhance: ({ app }) => {
--- docs/.vuepress/.temp/markdown-tab/config.js
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -1,6 +1,6 @@
-import { CodeTabs } from "F:/ownProject/huangjia/github-project/vue-press-02/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
-import { Tabs } from "F:/ownProject/huangjia/github-project/vue-press-02/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
-import "F:/ownProject/huangjia/github-project/vue-press-02/vuepress-starter/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+import { CodeTabs } from "F:/ownProject/huangjia/github-project/vue-press-02/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "F:/ownProject/huangjia/github-project/vue-press-02/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "F:/ownProject/huangjia/github-project/vue-press-02/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
 
 export default {
   enhance: ({ app }) => {
